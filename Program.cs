548ddc78c8faeecf3820f46589e3da36a80539cb
Proffi939 <proffi939@gmail.com>
2024-02-25 15:03:42 +0500

    create new array but not only write elem

--- Program.cs
+++ Program.cs
@@ -1,45 +1,52 @@
-﻿string [] CreateMass(int str)
+﻿string[] CreateArr(int str)
 {
-    string [] mass = new string [str];
+    string[] mass = new string[str];
     string inmass = "";
-    for (int i =0; i < str; i++)
+    for (int i = 0; i < str; i++)
     {
         Console.WriteLine("Введите элементы массива");
         inmass = Convert.ToString(Console.ReadLine()!);
         mass[i] = inmass;
-    } 
+    }
     return mass;
 }
-void MassPrint(string [] mass)
+string [] CreateNewArrLenStrLes(int len, string[] arr)
 {
-    Console.WriteLine("Ваш массив ");
-    foreach (string i in mass)
+    int count = 0;
+    foreach (string i in arr)
+    {
+        if (i.Length < len)
+        {
+            count++;
+        }
+    }
+    string [] newarr = new string [count];
+    int j = 0;
+    foreach(string e in arr)
+    {
+        if (e.Length < len)
+        {
+            newarr[j] = e;
+            j++;
+        }
+    }
+    return newarr;
+}
+void PrintArr(string [] arr)
+{
+    foreach(string i in arr)
     {
         Console.Write($"{i} ");
     }
 }
-void PrintStrLess3(string [] mass)
-{
-    Console.WriteLine("Массив с элементами меньше или равно 3");
-    int count = 0;
-    foreach(string e in mass)
-    {
-        if (e.Length < 4)
-        {
-            Console.Write($"{e} ");
-            count++;
-        }
-    }
-    if (count == 0)
-    {
-        Console.WriteLine("Элементов нет");
-    } 
-}
 Console.Clear();
 Console.WriteLine("Введите размер массива ");
 int len = int.Parse(Console.ReadLine()!);
-string [] mass = CreateMass(len);
+string[] arr = CreateArr(len);
+Console.WriteLine("Ваш массив:");
+PrintArr(arr);
 Console.WriteLine();
-MassPrint(mass);
-Console.WriteLine();
-PrintStrLess3(mass);
+Console.WriteLine("Введите значение длины строки меньше которой включаем в массив:");
+int less = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Результат:");
+PrintArr(CreateNewArrLenStrLes(less, arr));
